4ccd4237f3ca7fbe501e6702ed2495b91070208a
Tom <tomas.litherland@virginmedia.com>
2025-05-06 00:21:56 +0100

    Enable Warnings as Errors

--- build.lua
+++ build.lua
@@ -6,7 +6,7 @@ workspace "Campaign Tracker"
 
    -- Workspace-wide build options for MSVC
    filter "system:windows"
-      buildoptions { "/EHsc", "/Zc:preprocessor", "/Zc:__cplusplus" }
+      buildoptions { "/EHsc", "/Zc:preprocessor", "/Zc:__cplusplus", "/WX" }
 
 OutputDir = "%{cfg.system}-%{cfg.architecture}/%{cfg.buildcfg}"
 
