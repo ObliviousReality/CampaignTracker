8e986bea23ecfc971319c22429d8a43167c1702d
Tom <tomas.litherland@virginmedia.com>
2025-05-08 20:04:30 +0100

    Setup Berthog as example Character.

--- CampaignTrackerApp/src/App.cpp
+++ CampaignTrackerApp/src/App.cpp
@@ -18,29 +18,29 @@ void printDetails(Player * p, CTCore * core)
     printf("STR DEX CON INT WIS CHA\n");
     auto * abilities = p->getAbilities();
     printf(
-        " %i   %i   %i   %i   %i   %i\n",
-        abilities->STR,
-        abilities->DEX,
-        abilities->CON,
-        abilities->INT,
-        abilities->WIS,
-        abilities->CHA);
+        "+%i  +%i  +%i  +%i  +%i  +%i\n",
+        abilities->getModifier(AbilityType::Strength),
+        abilities->getModifier(AbilityType::Dexterity),
+        abilities->getModifier(AbilityType::Constitution),
+        abilities->getModifier(AbilityType::Intelligence),
+        abilities->getModifier(AbilityType::Wisdom),
+        abilities->getModifier(AbilityType::Charisma));
 
     printf("STR DEX CON INT WIS CHA\n");
     auto * savingThrows = p->getSavingThrows();
     printf(
-        " %i   %i   %i   %i   %i   %i\n",
-        savingThrows->STR,
-        savingThrows->DEX,
-        savingThrows->CON,
-        savingThrows->INT,
-        savingThrows->WIS,
-        savingThrows->CHA);
+        "+%i  +%i  +%i  +%i  +%i  +%i\n",
+        savingThrows->getModifier(AbilityType::Strength),
+        savingThrows->getModifier(AbilityType::Dexterity),
+        savingThrows->getModifier(AbilityType::Constitution),
+        savingThrows->getModifier(AbilityType::Intelligence),
+        savingThrows->getModifier(AbilityType::Wisdom),
+        savingThrows->getModifier(AbilityType::Charisma));
 
     printf("%i (%i) / %i\n", p->getHitPoints(), p->getTempHitPoints(), p->getHitPointsMax());
 
     printf("Init / Insp / AC\n");
-    printf("%i      %s    %i\n", p->getInitiative(), p->hasInspiration() ? "yes" : "no ", p->getAC());
+    printf("+%i      %s    %i\n", p->getInitiative(), p->hasInspiration() ? "yes" : "no ", p->getAC());
 
     printf("PER / INV / INS  SPEED\n");
     auto * passives = p->getPassives();
@@ -50,6 +50,37 @@ void printDetails(Player * p, CTCore * core)
         passives->investigation,
         passives->insight,
         p->getSpeed());
+
+    printf(" 1 2 3 4 5 6 7 8\n");
+    printf(" 0 0 0 0 0 0 0 0\n"); // Spell slots
+    printf("Conditions:\n");
+    for (const auto con : p->getConditions())
+    {
+        printf("%s, ", core->getConditionString(con).c_str());
+    }
+    printf("\n");
+    printf("Immunities:\n");
+    for (const auto con : p->getConditionImmunities())
+    {
+        printf("%s, ", core->getConditionString(con).c_str());
+    }
+    for (const auto dt : p->getDamageTypeImmunities())
+    {
+        printf("%s, ", core->getDamageTypeString(dt).c_str());
+    }
+    printf("\n");
+    printf("Resistances:\n");
+    for (const auto res : p->getResistances())
+    {
+        printf("%s, ", core->getDamageTypeString(res).c_str());
+    }
+    printf("\n");
+    printf("Senses:\n");
+    for (const auto sense : p->getSenses())
+    {
+        printf("%s, ", core->getSenseString(sense).c_str());
+    }
+    printf("\n");
 }
 
 int main()
@@ -59,39 +90,58 @@ int main()
 
     core->initalise();
 
-    std::cout << "Classes:" << std::endl;
-    for (const auto & c : core->getClasses())
-    {
-        std::cout << c.second->getName() << std::endl;
-    }
-    std::cout << "Races:" << std::endl;
-    for (const auto & c : core->getRaces())
-    {
-        std::cout << c.second->getName() << std::endl;
-    }
-    std::cout << "Conditions:" << std::endl;
-    for (const auto & c : core->getConditions())
-    {
-        std::cout << c.second->getName() << std::endl;
-    }
-    std::cout << "Damage Types:" << std::endl;
-    for (const auto & c : core->getDamageTypes())
-    {
-        std::cout << c.second->getName() << std::endl;
-    }
-    std::cout << "Senses:" << std::endl;
-    for (const auto & c : core->getSenses())
-    {
-        std::cout << c.second->getName() << std::endl;
-    }
+    //std::cout << "Classes:" << std::endl;
+    //for (const auto & c : core->getClasses())
+    //{
+    //    std::cout << c.second->getName() << std::endl;
+    //}
+    //std::cout << "Races:" << std::endl;
+    //for (const auto & c : core->getRaces())
+    //{
+    //    std::cout << c.second->getName() << std::endl;
+    //}
+    //std::cout << "Conditions:" << std::endl;
+    //for (const auto & c : core->getConditions())
+    //{
+    //    std::cout << c.second->getName() << std::endl;
+    //}
+    //std::cout << "Damage Types:" << std::endl;
+    //for (const auto & c : core->getDamageTypes())
+    //{
+    //    std::cout << c.second->getName() << std::endl;
+    //}
+    //std::cout << "Senses:" << std::endl;
+    //for (const auto & c : core->getSenses())
+    //{
+    //    std::cout << c.second->getName() << std::endl;
+    //}
 
     auto p = core->createPlayer(
-        "Gary",
-        ClassType::Fighter,
-        RaceType::Human,
-        "Gary",
-        1,
-        { MoralityType::Good, OrderType::Chaotic });
+        "Berthog",
+        ClassType::Warlock,
+        RaceType::Dwarf,
+        "Tom",
+        8,
+        { MoralityType::Good, OrderType::Lawful });
+
+    p->addSense(SenseType::Darkvision);
+    p->setAC(17);
+    p->setInitiative(3);
+    p->setSpeed(25);
+    p->setPassiveAbility(PassiveSkillType::Perception, 15);
+    p->setPassiveAbility(PassiveSkillType::Investigation, 18);
+    p->setPassiveAbility(PassiveSkillType::Insight, 12);
+    p->addResistance(DamageType::Poison);
+    p->setHitPoints(83);
+    p->setMaxHitPoints(83);
+    p->setTempHitPoint(5);
+
+    p->setAbility(AbilityType::Strength, 20);
+    p->setAbility(AbilityType::Dexterity, 17);
+    p->setAbility(AbilityType::Constitution, 18);
+    p->setAbility(AbilityType::Intelligence, 15);
+    p->setAbility(AbilityType::Wisdom, 14);
+    p->setAbility(AbilityType::Charisma, 15);
 
     printDetails(p, core.get());
 
---
+++ enc_temp_folder/50247cf6ef63e72ad2e1cf73e9bc8cc4/Creature.hpp
@@ -0,0 +1,197 @@
+#pragma once
+
+#include "data/Alignment.hpp"
+#include "data/Class.hpp"
+#include "data/Condition.hpp"
+#include "data/CreatureData.hpp"
+#include "data/DamageType.hpp"
+#include "data/Race.hpp"
+#include "data/Senses.hpp"
+
+#include <memory>
+#include <set>
+#include <string>
+
+class Creature
+{
+public:
+    Creature(const std::string name, const std::pair<MoralityType, OrderType> alignment);
+
+    const std::string getName() const { return name; }
+
+    const int getHitPoints() const { return hitPoints; }
+    const int getHitPointsMax() const { return hitPointsMax; }
+    const int getTempHitPoints() const { return tempHitPoints; }
+    const int getTotalHitPoints() const { return hitPoints + tempHitPoints; }
+    const int getAC() const { return armourClass; }
+    const int getSpeed() const { return speed; }
+    const int getInitiative() const { return initiative; }
+    const int getProficiency() const { return proficiency; }
+
+    void setHitPoints(const int newHP) { hitPoints = newHP; }
+    void setMaxHitPoints(const int newMaxHP) { hitPointsMax = newMaxHP; }
+    void setTempHitPoint(const int newTempHP) { tempHitPoints = newTempHP; }
+    void setAC(const int newAC) { armourClass = newAC; }
+    void setSpeed(const int newSpeed) { speed = newSpeed; }
+    void setInitiative(const int newInitiative) { initiative = newInitiative; }
+    void setProficiency(const int newProf) { proficiency = newProf; }
+
+    const int getAbility(const AbilityType at) const { return abilities->getAbility(at); }
+    const int getSavingThrow(const AbilityType st) const { return abilities->getAbility(st); }
+    const int getSkill(const SkillType st) const { return skills->getSkill(st); }
+    const int getPassiveAbility(const PassiveSkillType pst) { return passives->getPassiveSkill(pst); }
+
+    void setAbility(const AbilityType at, const int val) const { return abilities->setAbility(at, val); }
+    void setSavingThrow(const AbilityType st, const int val) const { return abilities->setAbility(st, val); }
+    void setSkill(const SkillType st, const int val) const { return skills->setSkill(st, val); }
+    void setPassiveAbility(const PassiveSkillType pst, const int val) { return passives->setPassiveSkill(pst, val); }
+
+    const bool hasAdvantage(const AbilityType at) { return abilityAdvantages.find(at) != abilityAdvantages.end(); }
+    const bool hasAdvantage(const SkillType st) { return skillAdvantages.find(st) != skillAdvantages.end(); }
+    const bool hasInitiativeAdvantage() { return initiativeAdvantage; }
+
+    const bool hasDisadvantage(const AbilityType at)
+    {
+        return abilityDisadvantages.find(at) != abilityDisadvantages.end();
+    }
+    const bool hasDisadvantage(const SkillType st) { return skillDisadvantages.find(st) != skillDisadvantages.end(); }
+    const bool hasInitiativeDisadvantage() { return initiativeDisadvantage; }
+
+    void setAdvantage(const AbilityType at, const bool val)
+    {
+        if (val)
+        {
+            abilityAdvantages.emplace(at);
+        }
+        else
+        {
+            abilityAdvantages.erase(at);
+        }
+    }
+
+    void setAdvantage(const SkillType st, const bool val)
+    {
+        if (val)
+        {
+            skillAdvantages.emplace(st);
+        }
+        else
+        {
+            skillAdvantages.erase(st);
+        }
+    }
+    void setInitiativeAdvantage(const bool val) { initiativeAdvantage = val; }
+
+    void setDisadvantage(const AbilityType at, const bool val)
+    {
+        if (val)
+        {
+            abilityDisadvantages.emplace(at);
+        }
+        else
+        {
+            abilityDisadvantages.erase(at);
+        }
+    }
+
+    void setDisadvantage(const SkillType st, const bool val)
+    {
+        if (val)
+        {
+            skillDisadvantages.emplace(st);
+        }
+        else
+        {
+            skillDisadvantages.erase(st);
+        }
+    }
+    void setInitiativeDisadvantage(const bool val) { initiativeDisadvantage = val; }
+
+    void addCondition(const ConditionType ct) { conditions.emplace(ct); }
+    void removeCondition(const ConditionType ct) { conditions.erase(ct); }
+    const bool hasCondition(const ConditionType ct) { return conditions.find(ct) != conditions.end(); }
+
+    void addImmunity(const ConditionType ct) { conditionImmunities.emplace(ct); }
+    void removeImmunity(const ConditionType ct) { conditionImmunities.erase(ct); }
+    const bool hasImmunity(const ConditionType ct) { return conditionImmunities.find(ct) != conditionImmunities.end(); }
+
+    void addImmunity(const DamageType dt) { damageTypeImmunities.emplace(dt); }
+    void removeImmunity(const DamageType dt) { damageTypeImmunities.erase(dt); }
+    const bool hasImmunity(const DamageType dt) { return damageTypeImmunities.find(dt) != damageTypeImmunities.end(); }
+
+    void addResistance(const DamageType dt) { resistances.emplace(dt); }
+    void removeResistance(const DamageType dt) { resistances.erase(dt); }
+    const bool hasResistance(const DamageType dt) { return resistances.find(dt) != resistances.end(); }
+
+    void addVulnerability(const DamageType dt) { vulnerabilities.emplace(dt); }
+    void removeVulnerability(const DamageType dt) { vulnerabilities.erase(dt); }
+    const bool hasVulnerability(const DamageType dt) { return vulnerabilities.find(dt) != vulnerabilities.end(); }
+
+    void addSense(const SenseType st) { senses.emplace(st); }
+    void removeSense(const SenseType st) { senses.erase(st); }
+    const bool hasSense(const SenseType st) { return senses.find(st) != senses.end(); }
+
+    const Abilities * const getAbilities() const { return abilities.get(); }
+    const Abilities * const getSavingThrows() const { return savingThrows.get(); }
+    const Skills * const getSkills() const { return skills.get(); }
+    const PassiveSkills * const getPassives() const { return passives.get(); }
+
+    const std::set<SkillType> & getSkillAdvantages() const { return skillAdvantages; }
+    const std::set<AbilityType> & getAbilityAdvantages() const { return abilityAdvantages; }
+
+    const std::set<SkillType> & getSkillDisadvantages() const { return skillDisadvantages; }
+    const std::set<AbilityType> & getAbilityDisadvantages() const { return abilityDisadvantages; }
+
+    const std::set<ConditionType> & getConditions() const { return conditions; }
+
+    const std::set<ConditionType> & getConditionImmunities() const { return conditionImmunities; }
+    const std::set<DamageType> & getDamageTypeImmunities() const { return damageTypeImmunities; }
+
+    const std::set<DamageType> & getResistances() const { return resistances; }
+    const std::set<DamageType> & getVulnerabilities() const { return vulnerabilities; }
+
+    const std::set<SenseType> & getSenses() const { return senses; }
+
+    const std::pair<MoralityType, OrderType> getAlignment() const { return { morality, order }; }
+
+    void setAlignment(const MoralityType newMorality, const OrderType newOrder)
+    {
+        morality = newMorality;
+        order = newOrder;
+    }
+
+private:
+    std::string name;
+
+    int hitPoints = 0;
+    int hitPointsMax = 0;
+    int armourClass = 0;
+    int speed = 30;
+    int initiative = 0;
+    int proficiency = 0;
+    int tempHitPoints = 0;
+
+    std::unique_ptr<Abilities> abilities;
+    std::unique_ptr<Abilities> savingThrows;
+    std::unique_ptr<Skills> skills;
+    std::unique_ptr<PassiveSkills> passives;
+
+    std::set<SkillType> skillAdvantages;
+    std::set<SkillType> skillDisadvantages;
+    std::set<AbilityType> abilityAdvantages;
+    std::set<AbilityType> abilityDisadvantages;
+    bool initiativeAdvantage = false;
+    bool initiativeDisadvantage = false;
+
+    std::set<ConditionType> conditions;
+    std::set<ConditionType> conditionImmunities;
+
+    std::set<DamageType> damageTypeImmunities;
+    std::set<DamageType> resistances;
+    std::set<DamageType> vulnerabilities;
+
+    std::set<SenseType> senses;
+
+    MoralityType morality;
+    OrderType order;
+};
