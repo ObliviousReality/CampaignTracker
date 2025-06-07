bdb58b996521c8f859236b466790c4fbe2c6277a
Tom <tomas.litherland@virginmedia.com>
2025-06-07 18:28:09 +0100

    Add Action class

---
+++ CampaignTrackerCore/src/data/Action.hpp
@@ -0,0 +1,94 @@
+#pragma once
+
+#include <string>
+
+enum class ActionLimitType
+{
+    PerDay,
+    RechargeRoll,
+    RechargeRest,
+    None,
+};
+
+class Action
+{
+public:
+    Action() {}
+
+    void setDescription(std::string newDesc) { description = newDesc; }
+    const std::string & getDescription() const { return description; }
+
+    void setLimitType(const ActionLimitType alt, const int val)
+    {
+        limitType = alt;
+
+        switch (alt)
+        {
+            case ActionLimitType::PerDay:
+            {
+                perDayCurrent = val;
+                perDayMax = val;
+                break;
+            }
+            case ActionLimitType::RechargeRoll:
+            {
+                rechargeRollMinimum = val;
+                break;
+            }
+            case ActionLimitType::RechargeRest: // nothing to do here, fallthrough
+            case ActionLimitType::None:
+            {
+                // Shouldn't occur
+                break;
+            }
+        }
+    }
+
+    void useAction()
+    {
+        switch (limitType)
+        {
+            case ActionLimitType::PerDay:
+            {
+                if (perDayCurrent)
+                {
+                    perDayCurrent--;
+                }
+                break;
+            }
+            case ActionLimitType::RechargeRoll:
+            case ActionLimitType::RechargeRest:
+            {
+                if (chargeAvailable)
+                {
+                    chargeAvailable = false;
+                }
+                // nothing to do here
+                break;
+            }
+            case ActionLimitType::None:
+            {
+                // Shouldn't occur
+                break;
+            }
+        }
+    }
+
+    bool canDoAction() { return (perDayCurrent > perDayMax) || chargeAvailable; }
+
+    void reset()
+    {
+        perDayCurrent = perDayMax;
+        chargeAvailable = true;
+    }
+
+private:
+    std::string description;
+    ActionLimitType limitType = ActionLimitType::None;
+
+    int perDayMax = 0;
+    int perDayCurrent = 0;
+
+    int rechargeRollMinimum = 5;
+    bool chargeAvailable = true;
+};
